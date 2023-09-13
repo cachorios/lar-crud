a4a816beb6dcd0b39b430d81087e87f21c39425f
cachorios <cachorios@gmail.com>
2023-09-13 17:33:02 -0300

    form section

--- stubs/livewire/resources/views/components/form-section.blade.php
+++ stubs/livewire/resources/views/components/form-section.blade.php
@@ -1,23 +1,15 @@
 @props(['submit'])
 
-@php
-if (isset($attributes['wire:submit']))
-    $submit = $attributes['wire:submit'];
-
-@endphp
-
-
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-4 md:gap-6']) }}>
-{{--    <div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>    --}}
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
     <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-section-title>
 
-    <div class="mt-5 md:mt-0 md:col-span-3">
+    <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit="{{ $submit }}">
             <div class="px-4 py-5 bg-white dark:bg-gray-800 sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
-                <div class="grid grid-cols-5 gap-6">
+                <div class="grid grid-cols-6 gap-6">
                     {{ $form }}
                 </div>
             </div>
